43fe1e90389777e16b8cb90a0c0b880e34fa77e7
Kacper Żarna <kacper@192.168.1.3>
2023-01-28 11:43:11 +0100

    Kod 28.01.2023 11:45

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import CountDisplay from "./components/CountDisplay/CountDisplay";
 import Display from "./components/Display/Display";
 import Form from "./components/Form/Form";
@@ -9,18 +9,25 @@ import TodoDisplay from "./components/TodoDisplay/TodoDisplay";
 // jsx - javascript expression
 // tsx
 function App() {
-  // symulacja fetcha
-  const response = {
-    firstName: "John",
-    lastName: "Smith",
-    age: 30,
-  };
+  // Hook - funkcja wbudowana (najczęściej) w react lub pochodząca od osób trzecich, ma ona dostęp do wewnętrznych mechanizmów działania reacta
+  // ZASADY HOOKÓW
+  // 1. Hooki mogą być używane tylko w komponentach funkcyjnych.
+  // 2. Hooki mogą być wywoływane tylko na top scopie w komponentach funkcyjnych.
+  // 3. Nazwy hooków zaczynają się na "use" (np. useState, useRef, useEffect, useForm itd.)
+  // useState - hook służący do przechowywania wartości, których zmiana ma spowodować przeładowanie się komponentu
+  // stan pusty => request => response => response do stanu => przeładowanie komponentu => wyświetlenie response
+  // useState zwraca listę z 2 elementami: zmienną stanową (stan) i funkcję aktualizującą zmienną stanową (stan)
+  // zmienną stanową możesz nazwać jak chcesz, w nazewnictwie funkcji aktualizującej zmienną stanową istnieje konwencja: set + nazwa zmiennej stanowej
+  const [count, setCount] = useState(0);
+  // count = 5 ZLE
+  // count++ ZLE
+  // setCount(5) DOBRZE
 
-  const todo = {
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
+  console.log("KOMPONENT SIĘ PRZEŁADOWAŁ");
+  const increaseCount = () => {
+    setCount(count + 1);
+    // count++ // to jest operacja, nie wartość, funkcja aktulizująca potrzebuje wartości
+    console.log(count);
   };
 
   return (
@@ -28,11 +35,15 @@ function App() {
       <Heading />
       <CountDisplay />
       <Manipulator />
-      <Form />
-      <Table response={response} />
-      {/* props = properties = własciwości */}
+      <div>
+        <h5>{count}</h5>
+      </div>
+      <button onClick={increaseCount}>Add 1</button>
+      {/* <Form /> */}
+      {/* <Table response={response} />
+      {/* props = properties = własciwości
       <Display response={response} />
-      <TodoDisplay todo={todo} />
+      <TodoDisplay todo={todo} /> */}
     </div>
   );
 }
@@ -51,3 +62,23 @@ export default App;
 
 // 1. Przekaż obiekt response do komponentu Table.tsx propsem. Nie zapomnij o odebraniu propsów, destrukturyzacji i intefejsie
 // 2. Wyświetl dane w tabeli (po lewej w tabeli imie nazwisko wiek, po prawej dane)
+
+// const response = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   age: 30,
+// };
+
+// const todo = {
+//   userId: 1,
+//   id: 1,
+//   title: "delectus aut autem",
+//   completed: false,
+// };
+
+// 1. Stwórz komponent DisplayRandom.
+// 2. W komponencie DisplayRandom stwórz stan randomNumber, wartość początkowa: 0
+// 3. Stwórz funkcję updateRandomNumber, w tej funkcji przy użyciu innej odpowiedniej funkcji aktulizuj stan randomNumber na losową liczbę.
+// 4. Stan randomNumber wyświetlaj w paragrafie w komponencie DisplayRandom.
+// 5. Stwórz przycisk z textem Update, na ten przycisk nadaj onClick, do onClicka podaj funkcję updateRandomNumber.
+// 6. Sam komponent DisplayRandom wyświetl w App.tsx
